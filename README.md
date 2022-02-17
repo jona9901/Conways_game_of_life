16758000a76b13b001b0de96d3a644ff5425d36e
jona9901 <castillojonathan068@gmai.com>
2022-02-16 23:25:36 -0600

    Read and applies the config file, also can take N and M as arguments from terminal

---
+++ README.md
@@ -0,0 +1,52 @@
+The game of life is a cellular automaton devised by the British mathematician John Horton Conway in 1970.
+
+The game is a zero-player game. It's evolution is determined by its initial state, requiring no furder input.
+
+One interacts with the game of life by creating a initial configuration and observing how it evolves.
+
+
+####################################################################################################
+
+RULES
+
+The universe of GoL is an infinite, two-dimensional orthogonbal grid of square cells, each of which is in one of two possible states, alive or dead.
+Every cell interacts with its eight neighbours (cells that are horizontally, vertically or diagonally adjacent), at each step in time.
+The following transitions occur:
+    # Any live cell with fewer than two live neighbours dies, underpopulation.
+    # Any live cell with two orthree live neighbours lives on to the next generation.
+    # Any live cell with more than three live neighbours dies, overpopulation.
+    # Any cell with exactly three live neighbours becomes a live cell, reproduction.
+
+
+These rules can be considered into the following:
+    # Any live cell with two or three neighbors survives.
+    # Any dead cell with three live neighbor becomes a live cell.
+    # All other lvie cells die in the next generation. Similarly, all other dead cells stay dead.
+
+
+####################################################################################################
+
+CONFIGURATIONS
+
+Common patter types:
+
+    # Still lifes   -   Do not change from one generation to the next.
+    # Oscillators   -   Return to their initial state after a finite number of generations.
+    # Spaceships    -   Translate themselves across the grid.
+
+
+
+####################################################################################################
+
+How to use:
+
+python3 conawys.py N M
+
+where N and M are the widht and height
+
+or 
+
+python3 conways.py
+
+
+The second option takes the N and M values from the 'config.txt' file
--- conway.py
+++ conway.py
@@ -12,15 +12,73 @@ ON = 255
 OFF = 0
 vals = [ON, OFF]
 
-def randomGrid(N):
-    """returns a grid of NxN random values"""
-    return np.random.choice(vals, N*N, p=[0.2, 0.8]).reshape(N, N)
+class Conway:
+    def __init__(self, file_name, args):
+        self.file_name = file_name
+        self.args = args
+        self.cells_allive = list()
+
+    class Cell:
+        def __init__(self, i, j):
+            self.i = i
+            self.j = j
+        def __str__(self):
+            print("i: %d, j: %d" % (self.i, self.j))
+        def graph(self, grid):
+            grid[self.i, self.j] = 1
+        def neighbour_sum(self, grid, N, M):
+            i = self.i
+            j = self.j
+
+            total = int((grid[i, (j-1)%M] + grid[i, (j+1)%M] +
+                         grid[(i-1)%N, j] + grid[(i+1)%N, j] +
+                         grid[(i-1)%N, (j-1)%M] + grid[(i-1)%N, (j+1)%M] +
+                         grid[(i+1)%N, (j-1)%M] + grid[(i+1)%N, (j+1)%M]))
+            return total
+
+    # read config file
+    def config(self):
+        with open(self.file_name) as f:
+            if (self.args):
+                self.N = int(self.args[0])
+                self.M = int(self.args[1])
+
+                f.readline().split()
+            else:
+                s = f.readline().split()
+
+                # Read widht and height
+                self.N = int(s[0])
+                self.M = int(s[1])
+
+            # Get generations
+            self.gens = f.readline()
+            for line in f:
+                s = line.split()
+                cell = self.Cell(int(s[0]),int(s[1]))
+                self.cells_allive.append(cell)
+
+            # Generate empty grid of N * M
+            #self.grid = randomGrid(self.N, self.M)
+            self.grid = np.zeros(self.N * self.M).reshape(self.M, self.N)
+            
+            # Graph cells allive
+            for cell in self.cells_allive:
+                cell.graph(self.grid)
+
+    def rules(self):
+        for cell in self.cells_allive:
+            print('neighbour sum: %d' % cell.neighbour_sum(self.grid, self.N, self.M))
+
+def randomGrid(N, M):
+    """returns a grid of NxM random values"""
+    return np.random.choice(vals, N*M, p=[0.2, 0.8]).reshape(N, M)
 
 def addGlider(i, j, grid):
-    """adds a glider with top left cell at (i, j)"""
+    """adds a glider with top left cell at (i, j)""" 
     glider = np.array([[0,    0, 255], 
-                       [255,  0, 255], 
-                       [0,  255, 255]])
+                        [255,  0, 255], 
+                        [0,  255, 255]])
     grid[i:i+3, j:j+3] = glider
 
 def update(frameNum, img, grid, N):
@@ -36,6 +94,28 @@ def update(frameNum, img, grid, N):
 
 # main() function
 def main():
+    args_buff = None
+    if(len(sys.argv) > 1):
+        args_buff = sys.argv[1:]
+    
+    cw = Conway('config.txt', args_buff)
+    cw.config()
+    cw.rules()
+
+    # set animation update interval
+    updateInterval = 50
+
+    # set up animation
+    fig, ax = plt.subplots()
+    img = ax.imshow(cw.grid, interpolation='nearest')
+    ani = animation.FuncAnimation(fig, update, fargs=(img, cw.grid, cw.N,),
+                                  frames = 10,
+                                  interval=updateInterval,
+                                  save_count=50)
+
+    plt.show()
+
+    """
     # Command line args are in sys.argv[1], sys.argv[2] ..
     # sys.argv[0] is the script name itself and can be ignored
     # parse arguments
@@ -44,6 +124,7 @@ def main():
     
     # set grid size
     N = 100
+    M = 100
         
     # set animation update interval
     updateInterval = 50
@@ -51,10 +132,10 @@ def main():
     # declare grid
     grid = np.array([])
     # populate grid with random on/off - more off than on
-    grid = randomGrid(N)
+    grid = randomGrid(N,M)
     # Uncomment lines to see the "glider" demo
     #grid = np.zeros(N*N).reshape(N, N)
-    #addGlider(1, 1, grid)
+    #WaddGlider(1, 1, grid)
 
     # set up animation
     fig, ax = plt.subplots()
@@ -65,6 +146,7 @@ def main():
                                   save_count=50)
 
     plt.show()
+    """
 
 # call main
 if __name__ == '__main__':
